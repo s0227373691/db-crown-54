f57d99c25af2a04c6800c535dcb4fa2e51e7afb3
Elmer <s0227373691@gms.tku.edu.tw>
2021-10-21 18:35:12 +0800

    use cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,12 +22,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({ origin: ['https://heroku-crown54.herokuapp.com/'] }))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/crown_54', crown_54_router);
 app.use('/crown2_54', crown2_54_router);
-app.use('/api_54', api_54_Router)
+app.use('/api_54', cors(), api_54_Router)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express',name: 'Elmer', id: '207410654' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express', name: '潘榆文', id: '207410654' });
 });
 
 module.exports = router;
